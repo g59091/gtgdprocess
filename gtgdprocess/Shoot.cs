117978e6a8c0cbbf3ace5e08cd9caea897ad1c5f
totally not gabe <agrian142476@gmail.com>
2018-12-13 01:07:28 -0500

    New C# Unity files

---
+++ gtgdprocess/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chp1
+{
+	// Every function by default is private in C#
+	public class Shoot : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			CheckForInput();
+		}
+
+		void CheckForInput() 
+		{
+			if (Input.GetKey(KeyCode.Mouse0))
+			{
+				Debug.Log("Key Pressed.");
+			}
+		}
+	}
+
+}
+
---
+++ gtgdprocess/Welcome.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// "namescape" allows for multiple files of the same name
+namespace Chp1
+{
+  public class Welcome : MonoBehaviour
+  {
+
+	  private string message = "Welcome";
+		private Text textWelcome;
+		public GameObject canvasWelcome;
+
+		// Awake function put before Start()
+		//void Awake(){}
+
+		// Use this for initialization
+		void Start()
+    {
+			SetInitialReferences();
+      WelcomeMessage();
+    }
+
+    // Update is called once per frame
+		/*
+		void Update()
+    {
+				
+    }
+		*/
+
+		void SetInitialReferences()
+		{
+			textWelcome = GameObject.Find("TextWelcome").GetComponent<Text>();
+		}
+
+    void WelcomeMessage()
+    {
+			if (textWelcome != null)
+				textWelcome.text = message;
+			else 
+				Debug.LogWarning("welcomeText not assigned.");
+
+			// Invoke = timer function to delay another function ["function_string, time_float"]
+			StartCoroutine(DisableCanvas(3.5f));
+		}
+
+		// IEnumerator used to suspend function with yield modifier
+		IEnumerator DisableCanvas(float waitTime)
+		{
+			// Every float needs f at the end
+			yield return new WaitForSeconds(waitTime);
+			canvasWelcome.SetActive(false);
+		}
+  }
+
+}
+
